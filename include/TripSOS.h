7060413e761926dde4176c8c32ba5c0cb442c5b3
akshaypandey10nov-create <akshay.pandey10nov@gmail.com>
2025-11-24 23:16:24 +0530

    Add TripSOS header file with function declarations

---
+++ include/TripSOS.h
@@ -0,0 +1,8 @@
+#ifndef TRIPSOS_H
+#define TRIPSOS_H
+
+void showHomePage(void);
+void showMenu(void);
+void showQuestions(int topic);
+void showSolution(int topic, int question);
+void showMapHint(int topic, int question);
